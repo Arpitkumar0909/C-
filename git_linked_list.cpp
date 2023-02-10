6a61ffb0346e568a71582cc9d80b516b3de7e7ec
arpit kuamr <arpitkuamr@gmail.com>
2023-02-10 10:41:48 +0530

    added a display function

--- git_linked_list.cpp
+++ git_linked_list.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class node
 {
+public:
     int data;
     node *next;
     node(int d)
@@ -17,10 +18,20 @@ void created_at_tail(node *&tail, int d)
     tail->next = temp;
     tail = temp;
 }
+void display(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << "-(" << temp->data << ")-";
+        temp = temp->next;
+    }
+}
 int main()
 {
     node *node1 = new node(1);
     node *head = node1;
     node *tail = node1;
+
     return 0;
 }
