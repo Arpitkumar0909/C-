732dcd596dbe9831bde5a25519e6a651f37b16c9
arpit kuamr <arpitkuamr@gmail.com>
2023-02-10 10:33:10 +0530

    created a node

--- git_linked_list.cpp
+++ git_linked_list.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
+class node
+{
+    int data;
+    node *next;
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
 
 int main()
 {
-    cout << "hi arpit we are good to go";
+    node *node1 = new node(1);
+    node *head = node1;
+    node *tail = node1;
     return 0;
 }
